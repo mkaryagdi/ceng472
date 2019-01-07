1afc3489dae42601694469f558f9ce6addf66974
Murat Karyağdı <murat@uncosoft.com>
2019-01-08 01:29:13 +0300

    Merge branch 'master' of https://github.com/mkaryagdi/ceng472
    # Conflicts:
    #	logs/application.log

--- app/controllers/api/NurseController.java
+++ app/controllers/api/NurseController.java
@@ -1,11 +1,15 @@
 package controllers.api;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jwt.filter.Attrs;
 import models.DoctorUser;
 import models.NurseUser;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.List;
 
 public class NurseController extends Controller {
 
@@ -14,9 +18,22 @@ public class NurseController extends Controller {
         NurseUser verifiedUser = request().attrs().get(Attrs.VERIFIED_NURSE_USER);
 
         if (verifiedUser.getId().equals(id)) {
-            return ok(Json.toJson(verifiedUser));
+            ObjectNode response = (ObjectNode) Json.toJson(verifiedUser);
+            response.set("doctors", getDocsAndPatiens(verifiedUser.getDoctorList()));
+            return ok(response);
         } else {
             return badRequest("You are not allowed!");
         }
+    }
+
+    private ArrayNode getDocsAndPatiens(List<DoctorUser> docList) {
+        ArrayNode docs = Json.newArray();
+        for (DoctorUser doctor : docList) {
+            docs.add(Json.newObject().put("name", doctor.getName()).put("surname", doctor.getSurname())
+                    .put("major", doctor.getMajor()).put("gender", doctor.getGender())
+                    .set("patients", doctor.getPatients()));
+        }
+
+        return docs;
     }
 }
